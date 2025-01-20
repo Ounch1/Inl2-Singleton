2d3c3b3077e0214c583108cdd312d90638b295ab
Ilja Grigorjev <tomkapisos@gmail.com>
2025-01-20 08:52:04 +0100

    Eager Initiation

--- RootUser.java
+++ RootUser.java
@@ -1,29 +1,26 @@
 public class RootUser {
-  private int UID = 0;
-  private int GID = 0;
-  private String userName = "root";
-  private String name = "Super user";
-  private String home = "/root";
-  private String shell = "/bin/sh";
-
-  private static RootUser instance;
-
-  private RootUser() {}
-
-  public static RootUser getInstance() {
-      if (instance == null) {
-          instance = new RootUser();
-      }
-      return instance;
+    private int UID = 0;
+    private int GID = 0;
+    private String userName = "root";
+    private String name = "Super user";
+    private String home = "/root";
+    private String shell = "/bin/sh";
+  
+    private final static RootUser INSTANCE = new RootUser();
+  
+    private RootUser() {}
+  
+    public static RootUser getInstance() {
+        return INSTANCE;
+    }
+  
+    @Override
+    public String toString() {
+        return new StringBuilder(userName)
+            .append("(").append(name).append(")")
+            .append(" ").append(UID).append(":").append(GID)
+            .append(" home: ").append(home).append(" ")
+            .append("shell: ").append(shell)
+            .toString();
+    }
   }
-
-  @Override
-  public String toString() {
-      return new StringBuilder(userName)
-          .append("(").append(name).append(")")
-          .append(" ").append(UID).append(":").append(GID)
-          .append(" home: ").append(home).append(" ")
-          .append("shell: ").append(shell)
-          .toString();
-  }
-}
