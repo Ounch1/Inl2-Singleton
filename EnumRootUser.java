4b291daaefd580d5cf91a9efaacd380e05010c8f
Ilja Grigorjev <ilja.grigorjev@volvo.com>
2025-05-23 14:00:41 +0200

    Implementing lazy and static instance singletons.

---
+++ EnumRootUser.java
@@ -0,0 +1,20 @@
+public enum EnumRootUser {
+    INSTANCE;
+
+    private final int UID = 0;
+    private final int GID = 0;
+    private final String userName = "root";
+    private final String name = "Super user";
+    private final String home = "/root";
+    private final String shell = "/bin/sh";
+
+    @Override
+    public String toString() {
+        return new StringBuilder(userName)
+            .append("(").append(name).append(")")
+            .append(" ").append(UID).append(":").append(GID)
+            .append(" home: ").append(home).append(" ")
+            .append("shell: ").append(shell)
+            .toString();
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        RootUser user = RootUser.INSTANCE;
-        System.out.println(user);
+        System.out.println("ENUM SINGLETON: ");
+        EnumRootUser enumUsr = EnumRootUser.INSTANCE;
+        System.out.println(enumUsr);
+
+        System.out.println("INSTANCE SINGLETON: ");
+        StaticInstanceRootUser staticUsr = StaticInstanceRootUser.getInstance();
+        System.out.println(staticUsr);
+
+        System.out.println("LAZY SINGLETON: ");
+        LazyRootUser lazyUsr = LazyRootUser.getInstance();
+        System.out.println(lazyUsr);
     }
-    
 }
